7ddf422478b79b67905be8faa19e38c92c371b2c
D33RS <Christoffer_hjort9090@hotmail.com>
2022-01-26 11:13:32 +0100

    Changed script
    Added debug line

--- Assets/NewScript.cs
+++ Assets/NewScript.cs
@@ -7,7 +7,9 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+     
         Debug.Log("Hello.");
+        Debug.Log("Fan vad kul vi ska ha dee nu ");
     }
 
     // Update is called once per frame
